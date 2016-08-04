7f01a5bd75dfa9b5df38f31cbe6ef555568bdbdf
Alexey Litvinov <sullenor@yandex-team.ru>
2016-08-04 15:07:11 +0300

    additional methods

--- page-object/Input.js
+++ page-object/Input.js
@@ -14,7 +14,71 @@ function Input() {
   }
 }
 
-Input.prototype = Object.create(Input.prototype, {
+Input.prototype = Object.create({
+  /**
+   * @param  {string} attributeName
+   * @return {string}
+   */
+  getAttribute: function (attributeName) {
+    assert(isString(attributeName));
+
+    this.selector = className(wrapper);
+    return getWebElement(this).getAttribute(null, attributeName);
+  },
+
+  /**
+   * @param  {string} attributeName
+   * @return {string}
+   */
+  getCssProperty: function (cssProperty) {
+    assert(isString(cssProperty));
+
+    this.selector = className(wrapper);
+    return getWebElement(this).getCssProperty(null, cssProperty);
+  },
+}, {
+  elementSize: {
+    get: function () {
+      this.selector = className(wrapper);
+      return getWebElement(this).getElementSize();
+    },
+  },
+
+  isDisabled: {
+    get: function () {
+      this.selector = className(control);
+      return getWebElement(this).getAttribute(null, 'disabled');
+    },
+  },
+
+  html: {
+    get: function () {
+      this.selector = className(control);
+      return getWebElement(this).getHtml();
+    },
+  },
+
+  name: {
+    get: function () {
+      this.selector = className(control);
+      return getWebElement(this).getAttribute(null, 'name');
+    },
+  },
+
+  tagName: {
+    get: function () {
+      this.selector = className(control);
+      return getWebElement(this).getTagName();
+    },
+  },
+
+  text: {
+    get: function () {
+      this.selector = className(wrapper);
+      return getWebElement(this).getText();
+    },
+  },
+
   value: {
     get: function () {
       this.selector = className(control);
