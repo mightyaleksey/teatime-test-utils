07857f42803b27a29a8b29588bd80efc6719436b
Alexey Litvinov <sullenor@yandex-team.ru>
2016-08-10 17:26:21 +0300

    added files from library to compilation step

--- preset.js
+++ preset.js
@@ -1,5 +1,10 @@
 'use strict';
 
+const { sep } = require('path');
+
+const isNodeModule = new RegExp(`${sep}node_modules${sep}`);
+const isTestUtil = new RegExp(`teatime-test-utils${sep}(page-object|tool)`);
+
 const cssModulesRequireHook = require('css-modules-require-hook');
 cssModulesRequireHook({
   generateScopedName: '[name]--[local]',
@@ -7,6 +12,8 @@ cssModulesRequireHook({
 
 const babelRegister = require('babel-register');
 babelRegister({
+  ignore: filename =>
+    !isTestUtil.test(filename) && isNodeModule.test(filename),
   presets: [
     'babel-preset-power-assert',
   ],
