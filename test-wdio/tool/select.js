0576a585a949d5d78fdb94cf3dd36876d36730ac
Alexey Litvinov <sullenor@yandex-team.ru>
2016-08-15 22:57:21 +0300

    simple select assertion

---
+++ test-wdio/tool/select.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const { loadSelectors } = require('../../tool/selector');
+const Select = loadSelectors('teatime-components/style/select/select.css');
+const assert = require('power-assert');
+
+before(() => browser.url('/select.html'));
+
+describe('Select', () => {
+  it('setValue', () => {
+    browser.click(`[name="cities"] + ${Select.control}`);
+    browser.waitForVisible(`[name="cities"] ~ ${Select.menu}`);
+
+    browser.click(`[name="cities"] ~ ${Select.menu} [data-value="barnaul"]`);
+    browser.waitForValue('[name="cities"]');
+    assert(browser.getValue('[name="cities"]'), 'barnaul');
+  });
+
+  it('setValue #searchable', () => {
+    browser.click(`[name="searchable-cities"] + ${Select.control}`);
+    browser.waitForVisible(`[name="searchable-cities"] ~ ${Select.menu}`);
+
+    browser
+      .click(`[name="searchable-cities"] ~ ${Select.menu} [data-value="perm"]`);
+    browser.waitForValue('[name="searchable-cities"]');
+    assert(browser.getValue('[name="searchable-cities"]'), 'perm');
+  });
+});
