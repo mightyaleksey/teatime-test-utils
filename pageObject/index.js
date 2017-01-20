840369144a071a60419c3945c9781d3b428559ed
Alexey Litvinov <sullenor@yandex-team.ru>
2017-01-20 17:23:33 +0300

    Added common index

---
+++ pageObject/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+exports.Button = require('./pageObject/Button');
+exports.Check = require('./pageObject/Check');
+exports.CheckGroup = require('./pageObject/CheckGroup');
+exports.ColorPicker = require('./pageObject/ColorPicker');
+exports.Input = require('./pageObject/Input');
+exports.Radio = require('./pageObject/Radio');
+exports.Select = require('./pageObject/Select');
+exports.Spin = require('./pageObject/Spin');
+exports.Textarea = require('./pageObject/Textarea');
+exports.Tumbler = require('./pageObject/Tumbler');
