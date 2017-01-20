7b79847a24db7c61721fa8455aadb201ad0bfd19
Alexey Litvinov <sullenor@yandex-team.ru>
2017-01-20 16:05:03 +0300

    getters and setters update

---
+++ test/templates/CheckGroup.snap.js
@@ -0,0 +1,15 @@
+import CheckGroup from 'teatime-components/component/CheckGroup';
+import React from 'react';
+
+export default () => (
+  <CheckGroup
+    name='checkGroup-control'
+    options={[
+      {label: 'Ducati', value: 'ducati'},
+      {label: 'Honda', value: 'honda'},
+      {label: 'Jawa', value: 'jawa'},
+    ]}/>
+);
+
+
+
