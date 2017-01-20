bcd5bfbd250d43a28df8f29f42a5195c4d572857
Alexey Litvinov <sullenor@yandex-team.ru>
2017-01-20 17:24:46 +0300

    f

--- pageObject/index.js
+++ pageObject/index.js
@@ -1,12 +1,12 @@
 'use strict';
 
-exports.Button = require('./pageObject/Button');
-exports.Check = require('./pageObject/Check');
-exports.CheckGroup = require('./pageObject/CheckGroup');
-exports.ColorPicker = require('./pageObject/ColorPicker');
-exports.Input = require('./pageObject/Input');
-exports.Radio = require('./pageObject/Radio');
-exports.Select = require('./pageObject/Select');
-exports.Spin = require('./pageObject/Spin');
-exports.Textarea = require('./pageObject/Textarea');
-exports.Tumbler = require('./pageObject/Tumbler');
+exports.Button = require('./Button');
+exports.Check = require('./Check');
+exports.CheckGroup = require('./CheckGroup');
+exports.ColorPicker = require('./ColorPicker');
+exports.Input = require('./Input');
+exports.Radio = require('./Radio');
+exports.Select = require('./Select');
+exports.Spin = require('./Spin');
+exports.Textarea = require('./Textarea');
+exports.Tumbler = require('./Tumbler');
