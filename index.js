e73716de961d6267d6ff32fc8e5a209829924440
Alexey Litvinov <sullenor@yandex-team.ru>
2017-01-20 17:07:47 +0300

    getValue and setValue

--- index.js
+++ index.js
@@ -1,12 +1,86 @@
 'use strict';
 
-const { getters } = require('./tool/getters');
-const { identities } = require('./tool/identities');
-const { setters } = require('./tool/setters');
+const {identify} = require('./lib/identify');
+const {getAttribute} = require('./lib/bro');
+const {propertyOf} = require('lodash/fp');
 
-const { getValue, identify, identifyElement, setValue } = require('./tool');
+const getClassName = selector => getAttribute(selector, 'class');
 
-exports.identify = identify(identities);
-exports.identifyElement = identifyElement(identities);
-exports.getValue = getValue(getters, exports.identifyElement);
-exports.setValue = setValue(setters, exports.identifyElement);
+const {
+  isCheck,
+  isCheckGroup,
+  isInput,
+  isRadio,
+  isSelect,
+  isTumbler,
+} = require('./lib/identity');
+const {
+  getCheckValue,
+  getCheckGroupValue,
+  getInputValue,
+} = require('./lib/getter');
+const {
+  setCheckValue,
+  setCheckGroupValue,
+  setInputValue,
+  setRadioValue,
+  setSelectValue,
+} = require('./lib/setter');
+
+const identities = {
+  Check: isCheck,
+  CheckGroup: isCheckGroup,
+  Input: isInput,
+  Radio: isRadio,
+  Select: isSelect,
+  Tumbler: isTumbler,
+};
+
+const getters = {
+  Check: getCheckValue,
+  CheckGroup: getCheckGroupValue,
+  Input: getInputValue,
+  Radio: getCheckValue,
+  Select: getInputValue,
+  Tumbler: getCheckValue,
+};
+
+const setters = {
+  Check: setCheckValue,
+  CheckGroup: setCheckGroupValue,
+  Input: setInputValue,
+  Radio: setRadioValue,
+  Select: setSelectValue,
+  Tumbler: setCheckValue,
+};
+
+const getValue = (mapOfGetters = getters, mapOfIdentities = identities) => {
+  const getControlGetter = propertyOf(mapOfGetters);
+  const getType = identify(mapOfIdentities);
+
+  return selector => {
+    const className = getClassName(selector);
+    const controlType = getType(className);
+    const controlGetter = getControlGetter(controlType);
+    return controlGetter(selector);
+  };
+};
+
+const setValue = (mapOfSetters = setters, mapOfIdentities = identities) => {
+  const getControlSetter = propertyOf(mapOfSetters);
+  const getType = identify(mapOfIdentities);
+
+  return (selector, value) => {
+    const className = getClassName(selector);
+    const controlType = getType(className);
+    const controlSetter = getControlSetter(controlType);
+    return controlSetter(selector, value);
+  };
+};
+
+exports.identities = identities;
+exports.getters = getters;
+exports.setters = setters;
+
+exports.getValue = getValue;
+exports.setValue = setValue;
